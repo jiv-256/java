5d2d70c598390c70f6d381615b2403c4a9e2d62c
R N pathak <rajiv.nayan2021@vitbhopal.ac.in>
2024-02-19 00:35:49 +0530

    Add files via upload

---
+++ 01_Basics/intro_02.java
@@ -0,0 +1,12 @@
+// comments
+// single line 
+/*multiline
+        comment
+ */
+
+// lets run out first java program 
+class first_program{
+    public static void main(String[] args) {
+        System.out.println("our first java program"); //print statement
+    }
+}
