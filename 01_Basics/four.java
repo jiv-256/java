71a5e22b78d790ba5a2c7a635f203edf28f4ae46
R N pathak <rajiv.nayan2021@vitbhopal.ac.in>
2024-02-20 00:15:31 +0530

    Add files via upload

---
+++ 01_Basics/four.java
@@ -0,0 +1,29 @@
+/*
+ * ***********************************  Keyboard    **************************************
+ *    ->Keyword are the reserved word whose meaning is already defined in the java compiler
+ *      
+ *      note-> we can't use keyword in our personal use
+ *           ->Keyword is a case-sensitive
+ * 
+ *       java keywords :-(java have 50 keywords + 3 literals)
+ * 
+ *         bytes    else        extends     import      Switch
+ *         short    for         implement   class       case
+ *         int      do          final       interface   const*
+ *         long     while       finally     new         goto*
+ *         float    break       try         native      strictfp**
+ *         double   continue    catch       instanceof  ehum****
+ *         void     default     throw       package     assert***
+ *         char     private     throws      return      abstract
+ *         boolean  protected   static      this        tranient
+ *         if       public      volatile    super       synhronized
+ * 
+ *         note_1 -> * (not used)
+ *                   **(added in 1.2v)
+ *                   ***(added 1.4 v)
+ *                   ****(added in 5.0 version)
+ * 
+ *         note_2 ->  null,true,false used as a literals in java
+ */
+
+
