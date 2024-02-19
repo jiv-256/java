8f176c4b9c832e61989672878108f3fb9185aa10
R N pathak <rajiv.nayan2021@vitbhopal.ac.in>
2024-02-20 00:12:27 +0530

    Add files via upload

---
+++ 01_Basics/second.java
@@ -0,0 +1,24 @@
+// Compilation and Execution process of JAVA in 4 simple step
+
+// step 1: First we make a java file with extension .java (lets say its name is java), it is also called source code
+// step 2:we give our first.java to java compiler
+// step 3:java compiler compiles .java file and make it .class file or byte code which provides us security and only jvm(java virtual machine can understand it) , its size is of 8 bits
+// step 4:Now we give our byte code to JVM and JVM gives us Output
+
+class rn{
+    public static void main(String[] args){
+        System.out.println("whats your name dude");
+    
+    }
+}
+
+/*
+ * lets take an overview of our syllabus which we are going to cover in this repository
+ * 
+ * ->   Datatype
+ * ->   Variable
+ * ->   Keyword
+ * ->   Identifier
+ * ->   Input & Output
+ * ->   Control flow
+ */
