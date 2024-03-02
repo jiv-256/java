fc1909b6d640c069adfa900fcfb47bfff7ef1a89
R N pathak <rajiv.nayan2021@vitbhopal.ac.in>
2024-03-02 18:39:57 +0530

    Add files via upload

---
+++ 01_Basics/five.java
@@ -0,0 +1,37 @@
+/*
+ **********************************     Identifier     *****************************************
+ *      -> Identifier refer to the name of variables,methods,class and so on which is given by the user
+ *      int a;  //here a is identifier
+ *      void ma();   //here ma is identifier
+ *      class A;     // here A is identifier
+ *      interfacec B;    //here B is identifier
+ * 
+ *      note-> all identifier is not variable but all variable is identifier
+ */
+
+ /*
+  * ****************************    Input & Output  *******************************************
+  *
+           ->We use Scanner class for input in java 
+            ->Scanner is a predefined class in util package of java 
+            -> input -> Scanner class(java.util.scanner)
+                                OR
+            -> input -> Scanner class(java.util.*)//After using star execution of program becomes slow because the compiler sccan the util package until it did't find scanner
+
+            Syntax :-
+            Scanner obj-name = new Scanner(System.in);
+            ->whenever we create object we always use new
+
+            scanner class methods 
+                1) nextInt()  //for integer value
+                2) nextLine() //for String value
+                3) nextDouble() //for double value
+                
+             Output -> 
+
+  */
+
+
+public class five {
+    
+}
